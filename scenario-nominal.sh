24a268dab104cffdfde0a1bec875d19dd5d9677f
Jerome Bernard <jerome.bernard@kalixia.com>
2019-01-15 12:10:41 +0100

    Ajout script pour simplifier la simulation.

---
+++ scenario-nominal.sh
@@ -0,0 +1,5 @@
+truffle exec --network eleveur_hauteluce_node nominal-lait-1.js
+truffle exec --network eleveur_parly_node nominal-lait-2.js
+truffle exec --network laiterie_beaufort_node nominal-laiterie.js
+truffle exec --network cooperative nominal-cooperative.js
+truffle exec --network eleveur_bastia_node nominal-pas-de-data.js
