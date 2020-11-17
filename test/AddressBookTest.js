da007c67aec6e0f6edbce05ba7215f3693861937
Alain Nicolas <alain.nicolas@talan.com>
2020-11-17 18:04:45 +0100

    Fix tests for exercise 1

--- test/AddressBookTest.js
+++ test/AddressBookTest.js
@@ -6,21 +6,21 @@ contract('AddressBook', function(accounts) {
   it("should detect geo boundaries properly", async () => {
     const addressBook = await AddressBook.deployed()
 
-    await addressBook.addParticipantZipCode(cheeseMaker, '0xaaa', 'quorum addr',73620, {from: cheeseMaker})
-    await addressBook.addParticipantZipCode(milkProducer1, '0xbbb', 'quorum addr', 73210, {from: cheeseMaker})
-    await addressBook.addParticipantZipCode(milkProducer2, '0xccc', 'quorum addr', 73270, {from: cheeseMaker})
-    await addressBook.addParticipantZipCode(milkProducer3, '0xddd', 'quorum addr', 20200, {from: cheeseMaker})
+    await addressBook.addParticipantZipCode('cheeseMaker', '0x809e815596abeb3764abf81be2dc39fbbacc9949', 'quorum addr',73620, {from: cheeseMaker})
+    await addressBook.addParticipantZipCode('milkProducer1', '0x809e815596abeb3764abf81be2dc39fbbacc9948', 'quorum addr', 73210, {from: cheeseMaker})
+    await addressBook.addParticipantZipCode('milkProducer2', '0x809e815596abeb3764abf81be2dc39fbbacc9947', 'quorum addr', 73270, {from: cheeseMaker})
+    await addressBook.addParticipantZipCode('milkProducer3', '0x809e815596abeb3764abf81be2dc39fbbacc9946', 'quorum addr', 20200, {from: cheeseMaker})
 
-    assert.equal(await addressBook.checkGeoBoundaries('0xaaa'), true, "the first participants should be in the correct geo boundaries")
-    assert.equal(await addressBook.checkGeoBoundaries('0xbbb'), true, "the first participants should be in the correct geo boundaries")
-    assert.equal(await addressBook.checkGeoBoundaries('0xccc'), true, "the first participants should be in the correct geo boundaries")
-    assert.equal(await addressBook.checkGeoBoundaries('0xddd'), false, "the fourth participant should NOT be in the correct geo boundaries")
+    assert.equal(await addressBook.checkGeoBoundaries('0x809e815596abeb3764abf81be2dc39fbbacc9949'), true, "the first participants should be in the correct geo boundaries")
+    assert.equal(await addressBook.checkGeoBoundaries('0x809e815596abeb3764abf81be2dc39fbbacc9948'), true, "the second participants should be in the correct geo boundaries")
+    assert.equal(await addressBook.checkGeoBoundaries('0x809e815596abeb3764abf81be2dc39fbbacc9947'), true, "the third participants should be in the correct geo boundaries")
+    assert.equal(await addressBook.checkGeoBoundaries('0x809e815596abeb3764abf81be2dc39fbbacc9946'), false, "the fourth participant should NOT be in the correct geo boundaries")
   })
 
   it("should only be allowed to add participants from the owner of the smart-contract", async () => {
     const addressBook = await AddressBook.deployed()
     try {
-      await addressBook.addParticipantZipCode(milkProducer1, '0xbbb', 'quorum addr', 73210, {from: milkProducer1})
+      await addressBook.addParticipantZipCode('Test', '0x809e815596abeb3764abf81be2dc39fbbacc9949', 'quorum addr', 73210, {from: milkProducer1})
       assert.fail()
     } catch (err) {
       assert.ok(/revert/.test(err.message))
